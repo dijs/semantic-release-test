d2b894509995c487e781f63a2e447fb0fe77b8a0
Richard van der Dys <richard.vanderdys@gmail.com>
2016-03-10 10:03:36 -0800

    fix: Fixed publish script

--- publish.sh
+++ publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-type=node node_modules/conventional-recommended-bump/cli.js -p angular
 
-echo $type
+type=`node node_modules/conventional-recommended-bump/cli.js -p angular`;
+current_version=`cat VERSION`;
+version=`node node_modules/semver/bin/semver $current_version -i $type`;
+
+echo $type;
+echo $current_version;
+echo $version;
